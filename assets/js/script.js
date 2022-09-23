d7686f96f5fe1465270ef1e4ac1928c3e1d247d6
EmmaRubiH <emmahjorting89@gmail.com>
2022-09-23 10:42:39 +0000

    change size on detail container. so it fit mobile device

--- assets/js/script.js
+++ assets/js/script.js
@@ -296,10 +296,10 @@ function closeOptionModal() {
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
+
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
+
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
